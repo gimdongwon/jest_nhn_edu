92bd2ae915b3099341484a7b11e0aaa6d6e2680f
kimdongwon <ehddnjs8989@gmail.com>
2020-02-02 18:46:08 +0900

    complete coverage test

--- src/serverCounter/counter.js
+++ src/serverCounter/counter.js
@@ -3,33 +3,33 @@ import axios from 'axios';
 import { createCounter } from '../backup/counter';
 
 export function createServerCounter(el, options) {
-  const $el = $(el);
-  const counter = createCounter(options);
+	const $el = $(el);
+	const counter = createCounter(options);
 
-  function render({ value, isMax, isMin }) {
-    const decDisabled = isMin ? 'disabled' : '';
-    const incDisabled = isMax ? 'disabled' : '';
+	function render({ value, isMax, isMin }) {
+		const decDisabled = isMin ? 'disabled' : '';
+		const incDisabled = isMax ? 'disabled' : '';
 
-    $el.html(`
-      <button type="button" ${decDisabled} class="btn btn-secondary btn-dec">-</button>
-      <span class="value">${value}</span>
-      <button type="button" ${incDisabled} class="btn btn-primary btn-inc">+</button>
+		$el.html(`
+      <button data-testid="btn-dec" type="button" ${decDisabled} class="btn btn-secondary btn-dec">-</button>
+      <span data-testid="value" class="value">${value}</span>
+      <button data-testid="btn-inc" type="button" ${incDisabled} class="btn btn-primary btn-inc">+</button>
     `);
-  }
+	}
 
-  axios.get('/counter').then(({ data }) => {
-    render(data);
-  });
+	axios.get('/counter').then(({ data }) => {
+		render(data);
+	});
 
-  $el.on('click', '.btn-inc', () => {
-    axios.put('/counter/inc').then(({ data }) => {
-      render(data);
-    });
-  });
+	$el.on('click', '.btn-inc', () => {
+		axios.put('/counter/inc').then(({ data }) => {
+			render(data);
+		});
+	});
 
-  $el.on('click', '.btn-dec', () => {
-    axios.put('/counter/dec').then(({ data }) => {
-      render(data);
-    });
-  });
+	$el.on('click', '.btn-dec', () => {
+		axios.put('/counter/dec').then(({ data }) => {
+			render(data);
+		});
+	});
 }
--- test/serverCounter.spec.js
+++ test/serverCounter.spec.js
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import { waitForDomChange, wait, getByText, fireEvent } from '@testing-library/dom';
+import { waitForDomChange, wait, getByText, fireEvent, getByTestId } from '@testing-library/dom';
 import { createServerCounter } from '../src/serverCounter/counter';
 
 const mockAxios = new MockAdapter(axios);
@@ -11,6 +11,9 @@ let container;
 beforeEach(async () => {
 	container = document.createElement('div');
 	document.body.appendChild(container);
+	mockAxios.onGet('/counter').reply(200, { value: 10, isMin: false, isMax: false });
+	createServerCounter(container);
+	await waitForDomChange({ container });
 });
 
 afterEach(() => {
@@ -19,22 +22,36 @@ afterEach(() => {
 });
 
 it('생성시 버튼과 초기값을 렌더링한다.', async () => {
-	mockAxios.onGet('/counter').reply(200, {
-		value: 10,
+	expect(getByTestId(container, 'btn-inc')).toBeVisible();
+	expect(getByTestId(container, 'btn-dec')).toBeVisible();
+	expect(getByTestId(container, 'value').textContent).toBe('10');
+});
+
+it('+ 버튼 클릭시 서버에 inc요청을 보낸 후 응답값으로 뷰를 갱신한다.', async () => {
+	mockAxios.onPut('/counter/inc').reply(200, {
+		value: 11,
 		isMin: false,
 		isMax: false
 	});
-	createServerCounter(container);
-	await waitForDomChange({ container });
+	fireEvent.click(getByTestId(container, 'btn-inc'));
 
-	expect(container.querySelect('.btn-inc')).toBeVisible();
-	expect(container.querySelect('.btn-dec')).toBeVisible();
-	expect(container.querySelect('.value')).toHaveTextContent('0');
+	await wait(() => {
+		expect(getByTestId(container, 'value').textContent).toBe('11');
+	});
 });
 
-it('+ 버튼 클릭시 서버에 inc요청을 보낸 후 응답값으로 뷰를 갱신한다.', async () => {});
+it('- 버튼 클릭시 서버에 dec 요청을 보낸 후 응답값으로 뷰를 갱신한다.', async () => {
+	mockAxios.onPut('/counter/dec').reply(200, {
+		value: 9,
+		isMin: false,
+		isMax: false
+	});
+	fireEvent.click(getByTestId(container, 'btn-dec'));
 
-it('- 버튼 클릭시 서버에 dec 요청을 보낸 후 응답값으로 뷰를 갱신한다.', async () => {});
+	await wait(() => {
+		expect(getByTestId(container, 'value').textContent).toBe('9');
+	});
+});
 
 it('최대값이면 + 버튼이 disabled 상태가 되고, 클릭해도 서버에 요청을 보내지 않는다', async () => {});
 
